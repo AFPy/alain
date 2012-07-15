e3d70381d41e52f22ccc99cbfb7a0ea99d164c71
Gael Pasgrimaud <gael@gawel.org>
2012-07-15 12:08:45 +0200

    lolitude

--- alain/regexp.py
+++ alain/regexp.py
@@ -3,8 +3,11 @@ REGEXP = (
 # syntaxe: (regexp, messages)
 # messages peut-etre une liste, un tuple, ou une chaine
 
+(r'^(lol|mdr)$',
+     ('MDR', 'hihihi', 'HAHAHA', "mdr t'es con"),
+
 (r'((^|.*\s)(faudrai.|faut|ca serait bien que)\s+qu.*)',
-                                        'WARNING !!! YAKAFOKON DETECTED !!!!'),
+     'WARNING !!! YAKAFOKON DETECTED !!!!'),
 
 (r'(.*\soffre.*emploi.*)',
     ('''Pour poster une offre d'emploi veuillez consulter:'''
