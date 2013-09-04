675b6a07ae8921f39205b8ed281a4c7db392841f
Gael Pasgrimaud <gael@gawel.org>
2013-09-04 13:08:45 +0200

    dont care of forums

--- alain/rss.py
+++ alain/rss.py
@@ -12,6 +12,7 @@ def awaiting():
     feed = feedparser.parse(
         'http://%s@www.afpy.org/search_rss?review_state=pending' % cred)
     entries = [str(e.id) for e in feed.entries]
+    entries = [e for e in entries if '/forums/' not in e]
     if entries:
         return 'Hey! Il y a des trucs à modérer: %s' % ' - '.join(entries)
     return ''
