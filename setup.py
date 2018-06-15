ec7a7da4c6ca01233dba00fcc4ffb91bc3a8d88c
Julien Palard <julien@palard.fr>
2018-06-15 18:20:29 +0200

    Add extras_require to get test requirements, reindent setup.py using black.

--- setup.py
+++ setup.py
@@ -1,27 +1,36 @@
 from setuptools import setup, find_packages
 import sys, os
 
-version = '0.1'
+version = "0.1"
 
-setup(name='alain',
-      version=version,
-      description="Alain",
-      long_description="""\
+setup(
+    name="alain",
+    version=version,
+    description="Alain",
+    long_description="""\
 AFPy's IRC bot""",
-      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
-      keywords='irc afpy',
-      author='Gael Pasgrimaud',
-      author_email='gawel@afpy.org',
-      url='https://hg.afpy.org/gawel/alain/index.html',
-      license='MIT',
-      packages=find_packages('src/'),
-      package_dir={'': 'src/'},
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
-          'irc3',
-          'chut',
-          'feedparser',
-          'requests',
-      ],
+    classifiers=[],  # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    keywords="irc afpy",
+    author="Gael Pasgrimaud",
+    author_email="gawel@afpy.org",
+    url="https://hg.afpy.org/gawel/alain/index.html",
+    license="MIT",
+    packages=find_packages("src/"),
+    package_dir={"": "src/"},
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=["irc3", "chut", "feedparser", "requests", "twitter"],
+    extras_require={
+        "dev": [
+            "flake8",
+            "pylint==2.0.0.dev",  # Don't work on py37 without v2
+            "astroid==2.0.0.dev",  # Don't work on py37 without v2
+            "pytest",
+            "pytest-cov",
+            "detox",
+            "bandit",
+            "black",
+            "mypy",
+        ]
+    },
 )
